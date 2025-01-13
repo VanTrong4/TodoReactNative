7d7fced4960762d16aae9e3f6e6d63822d57de5a
trongnvhbz <trongnvhbz@reactplus.com>
2025-01-13 18:58:04 +0700

    initial app

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  printWidth: 110,
+  tabWidth: 2,
   singleQuote: true,
   trailingComma: 'all',
+  endOfLine: 'auto',
 };
